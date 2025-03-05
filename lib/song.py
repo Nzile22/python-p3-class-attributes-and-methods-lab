c3b4e96deebef48e6a6fe8ffefb850e37dcc4525
Nzile22 <diananduku00@gmail.com>
2025-03-05 23:28:01 +0300

    initial commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,51 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}  # New attribute to track song counts for each artist
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        # Increment the count each time a new Song instance is created
+        Song.count += 1
+        # Add the genre and artist to their respective lists
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)  # Update artist count
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        # Add the genre to the genres list if it's not already present
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        # Add the artist to the artists list if it's not already present
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # Increment the count for the genre in the genre_count dictionary
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        # Increment the count for the artist in the artist_count dictionary
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} (Genre: {self.genre})"
+
